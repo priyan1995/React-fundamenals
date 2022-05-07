5b7fabdb0d04519ed54f522c96ee3415637efdc3
priyan <45042030+priyan1995@users.noreply.github.com>
2022-05-07 16:19:15 +0530

    bootstrap layout added with cards

--- src/components/React-bootstrap/LoginForm.jsx
+++ src/components/React-bootstrap/LoginForm.jsx
@@ -1,12 +1,73 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Alert, Card, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 const BootstrapLoginForm = () => {
-    return(
+    return (
         <div>
-            <h2>Bootstrap Login Form</h2>
-          <Button>test Button</Button>
+
+
+            <Container>
+
+                <h2 className='mt-5 mb-5'>Bootstrap Login Form</h2>
+
+
+                <Alert>
+                    This is an alert
+                </Alert>
+
+                <Row>
+                    <Col lg="4">
+                        <Card className="mb-3 mt-3">
+                            <Card.Img src="https://picsum.photos/200/300" className="pd-card-image" />
+                            <Card.Body>
+                                <Card.Title>
+                                    Card Example
+                                </Card.Title>
+                                <Card.Text style={{ color: "#343434" }}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis, similique veritatis laborum obcaecati sit consectetur exercitationem numquam ducimus incidunt sunt soluta, optio voluptatibus saepe quaerat, ullam porro doloribus pariatur.
+                                </Card.Text>
+                                <Button variant="primary">Read More</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col lg="4">
+                        <Card className="mb-3 mt-3">
+                            <Card.Img src="https://picsum.photos/200/300" className="pd-card-image" />
+                            <Card.Body>
+                                <Card.Title>
+                                    Card Example
+                                </Card.Title>
+                                <Card.Text style={{ color: "#343434" }}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis, similique veritatis laborum obcaecati sit consectetur exercitationem numquam ducimus incidunt sunt soluta, optio voluptatibus saepe quaerat, ullam porro doloribus pariatur.
+                                </Card.Text>
+                                <Button variant="primary">Read More</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                    <Col lg="4">
+                        <Card className="mb-3 mt-3">
+                            <Card.Img src="https://picsum.photos/200/300" className="pd-card-image" />
+                            <Card.Body>
+                                <Card.Title>
+                                    Card Example
+                                </Card.Title>
+                                <Card.Text style={{ color: "#343434" }}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis, similique veritatis laborum obcaecati sit consectetur exercitationem numquam ducimus incidunt sunt soluta, optio voluptatibus saepe quaerat, ullam porro doloribus pariatur.
+                                </Card.Text>
+                                <Button variant="primary">Read More</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                </Row>
+
+            </Container>
+
         </div>
     )
 }
