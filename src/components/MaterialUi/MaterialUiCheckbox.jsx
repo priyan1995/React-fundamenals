5a69b32ff6710013f9bcfda5a8602d354115227c
priyan <45042030+priyan1995@users.noreply.github.com>
2022-05-08 12:16:07 +0530

    MUI checkbox added

--- src/components/MaterialUi/MaterialUiCheckbox.jsx
+++ src/components/MaterialUi/MaterialUiCheckbox.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
-
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import AssignmentTurnedIn from '@material-ui/icons/AssignmentTurnedIn';
+import AssignmentLate from '@material-ui/icons/AssignmentLate';
 
 export const MaterialUiCheckbox = () => {
     const [checked, setChecked] = useState(true)
     return (
         <div>
-            <Checkbox
-                checked={checked}
-                onChange={(e) => setChecked(e.target.checked)}
-                inputProps={{
-                    'area-label': 'secondary checkbox'
-                }}
+            <FormControlLabel
+                control={<Checkbox
+                    checked={checked}
+                    checkedIcon={<AssignmentTurnedIn />}
+                    icon={<AssignmentLate />}
+                    onChange={(e) => setChecked(e.target.checked)}
+                    inputProps={{
+                        'area-label': 'secondary checkbox'
+                    }}
+                    color="secondary"
+                />}
+                label="I accept with with terms"
+
             />
         </div>
     )
