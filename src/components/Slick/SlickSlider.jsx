0489e449dbffe23cf7e4b189de6599cd0d35e484
PriyanIH <priyan.abewickrama@ideahubtech.com>
2022-05-13 12:23:24 +0530

    react-slick added

--- src/components/Slick/SlickSlider.jsx
+++ src/components/Slick/SlickSlider.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick/lib/slider';
 
 export const SlickSlider = () => {
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+
     return (
         <>
             <Container>
                 <h2>Slick Slider</h2>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                </Slider>
             </Container>
         </>
     )
