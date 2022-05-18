05f6c81a4179ce7e835843a30fa37a88d283b9ca
priyan <45042030+priyan1995@users.noreply.github.com>
2022-05-18 13:07:50 +0530

    react fragment note

--- src/components/Lazy-Load/LazyLoad.jsx
+++ src/components/Lazy-Load/LazyLoad.jsx
@@ -4,6 +4,7 @@ const LazyLoadContent = lazy(() => import('./LazyLoadContent'));
 
 export const LazyLoadComponent = () => {
     return (
+        // <> </> ==> this is react fragment shorthand syntax <React.Fragment>
         <>
             <h2>Code Splitting</h2>
             <Suspense fallback={<h1>Loading...</h1>}>
