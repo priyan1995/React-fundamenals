47034e303c96a4ba59e05323fd411e0c6b446990
priyan <45042030+priyan1995@users.noreply.github.com>
2022-05-22 18:30:15 +0530

    double adding operations restricted

--- src/calculator/index.jsx
+++ src/calculator/index.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './calculator.css';
 
 export const Calculator = () => {
 
+    const [calc, setCalc] = useState("");
+    const [result, setResult] = useState("");
+
+    const ops = ["+","/","*","-","0"]
+
+
+    const updateCalc = (value) => {
+
+        if(
+            ops.includes(value) && calc === '' || 
+            ops.includes(value) && ops.includes(calc.slice(-1))
+        ){
+            return;
+        }
+
+        setCalc(calc+value);
+    }
+
+
     const createDigits = () => {
+
         const digits = [];
 
-        for (let i; i < 10; i++) {
+        for (let i=1; i < 10; i++) {
             digits.push(
-                <button key={i}>{i}</button>
+                <button 
+                onClick={()=>updateCalc(i.toString())}
+                key={i}>
+                    {i}
+                    </button>
             )
         }
 
@@ -22,22 +46,22 @@ export const Calculator = () => {
                 <div className="pd-calculator">
 
                     <div className="pd-calc-display">
-                        <span>(0)</span>
-                        <span>0</span>
+                       { result ? <span>({result})</span> : ''} 
+                        <span>{calc || "0"}</span>
                     </div>
 
                     <div className="pd-calc-operators">
-                        <button>/</button>
-                        <button>*</button>
-                        <button>-</button>
-                        <button>+</button>
-                        <button>DEL</button>
+                        <button onClick={ ()=> updateCalc('/')}>/</button>
+                        <button onClick={ ()=> updateCalc('*')}>*</button>
+                        <button onClick={ ()=> updateCalc('-')}>-</button>
+                        <button onClick={ ()=> updateCalc('+')}>+</button>
+                        <button >DEL</button>
                     </div>
 
                     <div className="pd-calc-digits">
-                        {createDigits}
-                        <button>0</button>
-                        <button>.</button>
+                        {createDigits()}
+                        <button  onClick={ ()=> updateCalc('0')}>0</button>
+                        <button  onClick={ ()=> updateCalc('.')}>.</button>
                         <button>=</button>
                     </div>
 
