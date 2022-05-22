61d26996516162ca0118501d00c768cc2f8f98a1
priyan <45042030+priyan1995@users.noreply.github.com>
2022-05-22 18:04:11 +0530

    calculator elements added

--- src/calculator/index.jsx
+++ src/calculator/index.jsx
@@ -2,9 +2,48 @@ import React from 'react';
 import './calculator.css';
 
 export const Calculator = () => {
+
+    const createDigits = () => {
+        const digits = [];
+
+        for (let i; i < 10; i++) {
+            digits.push(
+                <button key={i}>{i}</button>
+            )
+        }
+
+        return digits;
+    }
+
     return (
         <>
-            <h2>Calculator</h2>
+
+            <div className="pd-calc-wrapper">
+                <div className="pd-calculator">
+
+                    <div className="pd-calc-display">
+                        <span>(0)</span>
+                        <span>0</span>
+                    </div>
+
+                    <div className="pd-calc-operators">
+                        <button>/</button>
+                        <button>*</button>
+                        <button>-</button>
+                        <button>+</button>
+                        <button>DEL</button>
+                    </div>
+
+                    <div className="pd-calc-digits">
+                        {createDigits}
+                        <button>0</button>
+                        <button>.</button>
+                        <button>=</button>
+                    </div>
+
+                </div>
+            </div>
+
         </>
     )
 }
