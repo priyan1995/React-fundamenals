6a6503201fa89f9ca399dddbc53ef4fa7420e932
priyan <priyan.abewickrama@ideahubtech.com>
2022-05-27 23:49:26 +0530

    firebase added

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp()
 
 export const Chat = () => {
     return(
