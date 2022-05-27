0cf991e0932d55f4723feaa49d2d4c31ecd71474
priyan <priyan.abewickrama@ideahubtech.com>
2022-05-28 00:00:19 +0530

    firebase initialization

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -3,7 +3,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-firebase.initializeApp()
+firebase.initializeApp({
+    apiKey: "AIzaSyAOgPLp1P4CSKjQEBUsKSuz2aG9eFQ6yh4",
+    authDomain: "react-chat-590d9.firebaseapp.com",
+    projectId: "react-chat-590d9",
+    storageBucket: "react-chat-590d9.appspot.com",
+    messagingSenderId: "334471822027",
+    appId: "1:334471822027:web:83c6ab7de1cd054232394e"
+})
 
 export const Chat = () => {
     return(
