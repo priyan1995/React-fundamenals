0d8e336a6295cbd9235f91cf2e35bf27c78c4279
priyan <priyan.abewickrama@ideahubtech.com>
2022-05-28 12:45:20 +0530

    google login authentication success

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from 'firebase/compat/app';
 // import { FirebaseApp } from 'firebase/app';
 import 'firebase/compat/auth';
@@ -6,8 +6,9 @@ import 'firebase/auth';
 import 'firebase/compat/firestore';
 import { ChatLoginButton } from '../components/Chat/ChatLoginButton';
 import { getAuth } from 'firebase/auth';
+import { Unsubscribe } from '@material-ui/icons';
 
-const auth = getAuth();
+
 
 firebase.initializeApp({
     apiKey: "AIzaSyAOgPLp1P4CSKjQEBUsKSuz2aG9eFQ6yh4",
@@ -18,25 +19,60 @@ firebase.initializeApp({
     appId: "1:334471822027:web:83c6ab7de1cd054232394e"
 })
 
+const auth = firebase.auth();
+
 export const Chat = () => {
 
-    const signInWithGoogle = async() => {
+    const [user, setUser] = useState(() => auth.currentUser);
+    const [initializing, setInitializing] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            if (initializing) {
+                setInitializing(false);
+            }
+        });      
+
+        //cleanup subscription
+        return unsubscribe;
+    }, [])
+
+    const signInWithGoogle = async () => {
         //retrieve google provider object
         const provider = new firebase.auth.GoogleAuthProvider();
         //set language to the default browser preference
         auth.useDeviceLanguage();
         //start sign in process
-        try{
+        try {
             await auth.signInWithPopup(provider);
-        }catch(error){
+        } catch (error) {
             console.log(error);
         }
     }
 
-
-    return(
+    if (initializing) { return "Loading..."; }
+    return (
         <>
-        <ChatLoginButton onClick={signInWithGoogle}>Sign In With Google</ChatLoginButton>
+            {
+                user ? (
+                    <>
+                        <h4>Welcome to the chat</h4>
+                    </>
+
+                ) : (
+
+                    <>
+                        <ChatLoginButton onClick={signInWithGoogle}>Sign In With Google</ChatLoginButton>
+                    </>
+
+                )
+            }
+
         </>
     )
 }
