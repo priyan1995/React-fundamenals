812940d149a923dce68575d0329971487a54ed35
priyan <priyan.abewickrama@ideahub.lk>
2022-06-02 11:41:27 +0530

    custom modal added

---
+++ src/components/Common-Components/Modal.jsx
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react';
+
+export const CustomModal = (props) => {
+
+    const modalRef = useRef();
+
+    const showModal = props.showModal;
+    const setShowModal = props.setShowModal;
+
+    const closeModal = (e) => {
+        if (e.target === modalRef.current) {
+            setShowModal(false);
+        }
+    }
+
+    return (
+        <>
+            {showModal ? (
+                <>
+                    <div className='pd-modal' ref={modalRef} onClick={closeModal}>
+                        <div className='container'>
+                            {props.children}
+                            <button onClick={() => setShowModal(false)}>
+                                X
+                            </button>
+                        </div>
+                    </div>
+                </>
+            ) : ''}
+
+        </>
+    )
+}
--- src/components/Todo/AddNewTodo.jsx
+++ src/components/Todo/AddNewTodo.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CustomModal } from '../Common-Components/Modal';
 
 export const AddNewTodo = () => {
+
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <>
-            <h2>AddNewTodo</h2>
+            <div className='pd-add-new-todo'>
+                <button className='btn' onClick={() => setShowModal(true)}> + New Todo</button>
+
+                <CustomModal showModal={showModal} setShowModal={setShowModal}>
+                    <h4>Add New Todo</h4>
+                </CustomModal>
+            </div>
+
         </>
     )
 }
