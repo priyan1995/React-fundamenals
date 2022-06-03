d89ed46d59375a723cced62f345e3b6beca8971b
priyan <priyan.abewickrama@ideahub.lk>
2022-06-03 09:41:49 +0530

    calendar - menu bar added

--- src/components/Todo/Calendar.jsx
+++ src/components/Todo/Calendar.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons';
 
 export const Calendar = () => {
+
+    const calendarItems = ['today', 'next 7 days', 'all days'];
     return (
         <>
-            <h2>Calendar</h2>
+            <div className='calendar'>
+                <div className='header'>
+                    <div className='title'>
+                        <p>Calendar</p>
+                        <CalendarDate size='18' />
+                    </div>
+                    <div className='btns'>
+                        <span>
+                            <CaretUp size='20' />
+                        </span>
+                    </div>
+                </div>
+
+                <div className='items'>
+                    {
+                        calendarItems.map(item =>
+                            <div className='item' key={item}>
+                                {item}
+                            </div>
+                        )
+                    }
+                </div>
+
+            </div>
         </>
     )
 }
