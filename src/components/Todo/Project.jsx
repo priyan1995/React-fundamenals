8a18d702de61beb44ff62e30692c2b2fad0f80f1
priyan <priyan.abewickrama@ideahub.lk>
2022-06-04 11:44:48 +0530

    projects list added

--- src/components/Todo/Project.jsx
+++ src/components/Todo/Project.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Pencil, XCircle } from 'react-bootstrap-icons';
 import { ProjectRename } from './ProjectRename';
 
-export const Project = () => {
+export const Project = (props) => {
+    const project = props.project
     return (
         <>
-           <ProjectRename />
+            <div className='pd-project'>
+
+                <div className='name'>
+                    {project.name}
+                </div>
+
+                <div className='btns'>
+                    <div className='edit-delete'>
+                        <span>
+                            <Pencil size="13" />
+                        </span>
+                        <span>
+                            <XCircle size="13" />
+                        </span>
+                    </div>
+
+                    <div className='total-todos'>
+                        {project.numOfTodos}
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
--- src/components/Todo/Projects.jsx
+++ src/components/Todo/Projects.jsx
@@ -6,11 +6,13 @@ import { CaretUp, Palette, PencilFill } from 'react-bootstrap-icons';
 export const Projects = () => {
 
     const [showMenu, setShowMenu] = useState(true);
+    const [edit, setEdit] = useState(false);
+    const pencilColor = edit ? "#1EC94C" : "#000000"
 
     const projects = [
-        { id:1, name:"personal", numOfTodos:0 },
-        { id:2, name:"work", numOfTodos:1 },
-        { id:3, name:"other", numOfTodos:2 },
+        { id: 1, name: "personal", numOfTodos: 0 },
+        { id: 2, name: "work", numOfTodos: 1 },
+        { id: 3, name: "other", numOfTodos: 2 },
     ]
 
     return (
@@ -24,20 +26,37 @@ export const Projects = () => {
 
                     <div className='btns'>
                         {
-                            showMenu &&
+                            showMenu && projects.length > 0 &&
 
-                            <span>
-                                <PencilFill size='15' />
+                            <span className='edit' onClick={() => setEdit(!edit)}>
+                                <PencilFill size='15' color={pencilColor} />
                             </span>
 
                         }
 
                         <AddNewProjects />
-                        <span>
+
+                        <span className='arrow'>
                             <CaretUp size='20' />
                         </span>
+
                     </div>
                 </div>
+
+                <div className='items'>
+
+                    {
+                        projects.map(project => 
+                            <Project
+                                project={project}
+                                key={project.id}
+                                edit={edit}
+                            />
+                        )
+                    }
+
+                </div>
+
             </div>
         </>
     )
