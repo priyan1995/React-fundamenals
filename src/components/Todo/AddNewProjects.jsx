a2db643a5dd0e3383fd5b9b47fdb958167a36d28
priyan <priyan.abewickrama@ideahub.lk>
2022-06-05 21:59:44 +0530

    new project adding form

--- src/components/Todo/AddNewProjects.jsx
+++ src/components/Todo/AddNewProjects.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Plus } from 'react-bootstrap-icons';
+import { CustomModal } from '../Common-Components/Modal';
 
 export const AddNewProjects = () => {
+
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <>
             <div className='pd-add-new-project'>
                 <div className='add-button' >
-                    <span>
-                        <Plus size='20' />
+                    <span onClick={()=>setShowModal(true)}>
+                        <Plus size='20'  />
                     </span>
                 </div>
+                <CustomModal showModal={showModal} setShowModal={setShowModal}>
+
+                </CustomModal>
             </div>
         </>
     )
